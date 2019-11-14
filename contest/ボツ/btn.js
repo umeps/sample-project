e0938d8e9c44a28ae14fde38d5b0efb7a2f8c2ae
umeps <[umepsjp@gmail.com]>
2019-11-14 18:51:43 +0900

    modal 作成

--- contest/ボツ/btn.js
+++ contest/ボツ/btn.js
@@ -1,20 +1,39 @@
-$(document).on('click', function(e) {
-    if(!$(e.target).closest('#pop_up').length && !$(e.target).closest('#button').length){
-        $('#pop_up').fadeOut();
-    }else if($(e.target).closest('#button').length){
-        if($('#pop_up').is(':hidden')){
-            $('#pop_up').fadeIn();
-        }else{
-            $('#pop_up').fadeOut();
-        }
-    }
-});
-
 
 $(document).ready(function(){
-    $("a").click(function(){
-      $("#popup").fadeToggle( "slow" );
+    $(".access").click(function(){
+        for(let i=1;i<=2; i++){
+            if (i != 1){
+            $('#' + i).hide();
+            }
+        }
+      $("#1").fadeToggle( "slow" );
+    });
       
+  });
+
+
+  $(document).ready(function(){
+    $(".maill").click(function(){
+        for(let i=1;i<=2; i++){
+            if (i != 2){
+            $('#' + i).hide();
+            }
+        }
+      $("#2").fadeToggle( "slow" );
+    });
+      
+  });
+
+
+
+  $(document).ready(function(){
+    $(".modal10").click(function(){
+        // for(let i=1;i<=2; i++){
+        //     if (i != 2){
+        //     $('#' + i).hide();
+        //     }
+        // }
+      $("#2").fadeToggle( "slow" );
     });
       
   });
